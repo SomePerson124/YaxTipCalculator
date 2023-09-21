cbdb5ab78407a33df726d5421e044f4b4e2eb0d9
Gavin <Gavinn8@nycstudents.net>
2023-09-21 10:35:11 -0400

    code segment added to add 1 to prevent -1 being added to total, added double totalTip to calculate tip, added print statements to display valuable information

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -20,7 +20,16 @@ public class TipCalculator {
             System.out.print("Enter a cost in dollars and cents: ");
             cost = scan.nextDouble();
             total += cost;
+            total ++;
         }
+
+        double totalTip = total * (tipPercentage / 100.0);
+
+        System.out.print("------------------------------------------------");
+        System.out.println("Total bill before tip: $" + total);
+        System.out.println("Total Percentage: " + tipPercentage + "%");
+        System.out.println("Total tip: $" + totalTip);
+        System.out.println("Total bill with tip: $" + (total + totalTip));
 
     }
 }
